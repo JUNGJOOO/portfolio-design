3acc6c2a60c3ca8cc6af188d8a3a9d234ed721cd
JUNGJOOO <42334030+JUNGJOOO@users.noreply.github.com>
2020-05-12 18:38:17 +0900

    Add files via upload

---
+++ js/depu.js
@@ -0,0 +1,18 @@
+// var burger = $('.menu-trigger');
+        
+// burger.each(function(index){
+//     var $this = $(this);
+    
+//     $this.on('click', function(e){
+//         e.preventDefault();
+//         $(this).toggleClass('active-' + (index+1));
+//     })
+// });
+
+
+// var e = document.getElementById('all');
+// e.addEventListener('click', function() {
+//     if (this.className == 'on') this.classList.remove('on');
+//     else this.classList.add('on');
+// });
+
